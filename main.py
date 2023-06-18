4677675379af49f89f261acf50d3ecf0e8433b9b
san5ezez <dexter.craft.boss@gmail.com>
2023-06-18 11:37:45 +0600

    Dis_Bot_Kodland

--- main.py
+++ main.py
@@ -1,28 +1,79 @@
 import discord
+from discord.ext import commands
+from discord import *
+import asyncio
+import requests
+import os
+import random
+img = os.listdir('images')
 
-token =  "Token"
-# Переменная intents - хранит привилегии бота
+token =  "MTExMjI5NDQ5MTUwNDc4NzU1Nw.Gi99_r.JoIQTmBOz_nVo78yCfYdTqFDgOTkOFXVhT0-Gk"
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.command()
+async def mem(ctx):
+    random_file = random.choice(img)
+    with open(f'images/{random_file}', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+
+
+def get_sobaka_image_url():    
+    url1 = 'https://random.dog/woof.json' 
+    res = requests.get(url1)
+    data = res.json()
+    print(data)
+    return data['url']
+
+
+@bot.command('sobaka')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_sobaka_image_url()
+    await ctx.send(image_url)
+
+
+
+def get_lisa_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+@bot.command(name='lisa')
+async def lisa(ctx):
+    image_url = get_lisa_image_url()
+    await ctx.send(image_url)
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$aaaaaa'):
-        await message.channel.send("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAW!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
-client.run(token)
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def rick_astly(ctx):
+    await ctx.send('https://tenor.com/view/rickroll-meme-internet-never-gonna-gif-26474110')
+
+@bot.command(name="timer")
+async def timer(ctx, number):
+    number = int(number)
+    message = await ctx.send(number)
+    while number != 0:
+        number -= 1
+        await message.edit(content=number)
+        await asyncio.sleep(1)
+    await message.edit(content='Ended!')
+
+bot.run(token)
